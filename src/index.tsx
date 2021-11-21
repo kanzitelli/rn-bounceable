60a063648974b23943cf4cab580c17213f2db4e1
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-11-21 16:23:33 +0100

    onLongPress, delays, noBounce and more added

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,26 @@ import Animated, {
   runOnJS,
 } from 'react-native-reanimated';
 import {TapGestureHandler, State} from 'react-native-gesture-handler';
+import {useMemo} from 'react';
 
-export type BouncableProps = {
+type PureFunc = () => void;
+export type BounceableProps = {
   disabled?: boolean;
-  onPress?: () => void;
+  noBounce?: boolean;
+  onPress?: PureFunc;
+  immediatePress?: boolean; // indicates if press action should be fired right after or w/ small delay (when end animation will be finished)
+  onLongPress?: PureFunc;
+  delayLongPress?: number;
   activeScale?: number;
+  delayActiveScale?: number;
   springConfig?: Animated.WithSpringConfig;
   contentContainerStyle?: StyleProp<ViewStyle>;
 };
 
-export const Bounceable: React.FC<BouncableProps> = ({
+export const Bounceable: React.FC<BounceableProps> = ({
   children,
   disabled = false,
+  noBounce = false,
   onPress,
   activeScale = 0.95,
   springConfig = {
@@ -27,8 +35,16 @@ export const Bounceable: React.FC<BouncableProps> = ({
     stiffness: 300,
   },
   contentContainerStyle,
+
+  delayLongPress = 800,
+  delayActiveScale = 0,
+  onLongPress,
+  immediatePress = true,
 }) => {
+  const onLongPressTimeoutId = useSharedValue<null | ReturnType<typeof setTimeout>>(null);
   const scale = useSharedValue(1);
+  const isActive = useSharedValue(0);
+
   const sz = useAnimatedStyle(() => {
     'worklet';
 
@@ -41,35 +57,99 @@ export const Bounceable: React.FC<BouncableProps> = ({
     };
   });
 
+  const beginScale = () => {
+    scale.value = withSpring(activeScale, springConfig);
+  };
+  const endScale = () => {
+    // clearing up
+    isActive.value = 0;
+    if (onLongPressTimeoutId.value !== null) {
+      clearTimeout(Number(onLongPressTimeoutId.value));
+      onLongPressTimeoutId.value = null;
+    }
+
+    scale.value = withSpring(1, springConfig);
+  };
+
+  const Children = useMemo(
+    () => <Animated.View style={[contentContainerStyle, sz]}>{children}</Animated.View>,
+    [contentContainerStyle, sz, children],
+  );
+
+  if (noBounce) {
+    return Children;
+  }
+
   return (
     <TapGestureHandler
+      maxDurationMs={99999999}
       shouldCancelWhenOutside={true}
       onHandlerStateChange={({nativeEvent}) => {
         if (disabled) {
           return;
         }
+
         const {state} = nativeEvent;
 
         if (state === State.BEGAN) {
-          scale.value = withSpring(activeScale, springConfig);
+          isActive.value = 1;
+
+          // delaying scale beginning
+          if (delayActiveScale <= 0) {
+            beginScale();
+          } else {
+            setTimeout(() => {
+              if (isActive.value === 1) {
+                beginScale();
+              }
+            }, delayActiveScale);
+          }
+
+          // onLongPress
+          if (onLongPress) {
+            onLongPressTimeoutId.value = setTimeout(() => {
+              if (isActive.value === 1) {
+                endScale();
+                runOnJS(onLongPress)();
+              }
+            }, delayLongPress);
+          }
+
           return;
         }
 
         if (state === State.END) {
-          if (onPress) {
-            runOnJS(onPress)();
+          if (onPress && isActive.value === 1) {
+            // mimicing bounce effect if delay active scale is set
+            if (delayActiveScale > 0) {
+              beginScale();
+            }
+
+            setTimeout(() => {
+              endScale();
+              if (!immediatePress) {
+                runOnJS(onPress)();
+              }
+            }, 50);
+
+            if (immediatePress) {
+              runOnJS(onPress)();
+            }
+
+            return;
           }
-          scale.value = withSpring(1, springConfig);
+
+          endScale(); // ending scaling here just in case
           return;
         }
 
         if (state === State.UNDETERMINED || state === State.FAILED || state === State.CANCELLED) {
-          scale.value = withSpring(1, springConfig);
+          endScale();
           return;
         }
       }}
     >
-      <Animated.View style={[contentContainerStyle, sz]}>{children}</Animated.View>
+      {Children}
     </TapGestureHandler>
   );
 };
